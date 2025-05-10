85598631d7eeb0462c309149849a4fc92c0233b7
TareQ <tareqhasan2007@gmail.com>
2025-05-10 21:22:34 +0600

    second

--- photo/urls.py
+++ photo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('view/<int:pk>/', views.viewPhoto, name='viewPhoto'),
+    path('add/', views.addPhoto, name='add'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'photo/index.html')
+
+
+def viewPhoto(request,pk):
+    return render(request, 'photo/view.html')
+
+
+def addPhoto(request):
+    return render(request, 'photo/add.html')
