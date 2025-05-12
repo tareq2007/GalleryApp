eb690e6e471e07861c9607336571deed8907d90a
TareQ <tareqhasan2007@gmail.com>
2025-05-12 14:04:54 +0600

    Collectstatic

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='photos/')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
